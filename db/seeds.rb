b6ad47e6c21502dc4e4239c13d4d462341bba623
westnorthern <rrichardson610@gmail.com>
2017-12-18 13:45:23 -0700

    basic workouts seed and workouts index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Workout.create(title: 'pushup', description: 'use your whole body to try and push the earth into the sun', video_url: 'pushups.com', min_lvl: 0, min_reps: 5)
+Workout.create(title: 'situp', description: 'lay on your back and bend at the middle', video_url: 'situps.com', min_lvl: 0, min_reps: 5)
+Workout.create(title: 'pullup', description: 'grab a fixed bar above your head and bend your arms until your head is over the bar', video_url: 'pullups.com', min_lvl: 5, min_reps: 1)
+Workout.create(title: 'mountain climber', description: 'from the front leaning rest position, run in place', video_url: 'pain.com', min_lvl: 5, min_reps: 5)
+Workout.create(title: 'side straddle hop', description: 'its a jumping jack', video_url: 'goarmy.com', min_lvl: 0, min_reps: 5)
+Workout.create(title: 'kung fu', description: 'do ten seconds of random kung fu!', video_url: 'wataaa.com', min_lvl: 0, min_reps: 5)
