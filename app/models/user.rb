34d6375fb60978b7d06b863a7eaaf608eb28c631
westnorthern <rrichardson610@gmail.com>
2017-12-20 17:21:54 -0700

    changed last_three to add modulo

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,6 +32,23 @@ class User < ApplicationRecord
 
   end
 
+  def is_hydrated
+  	today = current_day
+  	now = current_hour
+
+  	if self.hourly_scores.last
+  		if self.hourly_scores.last.created_at.utc.day == today
+  			if self.hourly_scores.last.created_at.utc.hour == current_hour
+  				if self.hourly_scores.last.hydrated == true
+  					return true
+  				else
+  					return false
+  				end
+  			end
+  		end
+  	end
+  	return false
+  end
 
 
   def last_three # returns array of the previous three hours
@@ -39,7 +56,7 @@ class User < ApplicationRecord
 		x = 3
 		arr = []
 		while x > 0
-			arr.push(now - x)
+			arr.push((now - x) % 23)
 			x -= 1
 		end
 		return arr
@@ -91,11 +108,14 @@ class User < ApplicationRecord
 
 
   	if self.hourly_scores.last
-  		scores_arr = self.hourly_scores.last(4).map { |x| x.created_at.utc.hour }
+  		scores_arr = self.hourly_scores.all.map { |x| x.created_at.utc.hour }
+  		puts "the scores array"
+  		p scores_arr
   		three_arr.each_with_index do |x, y|
   			if scores_arr.include? x
   				result_arr.push(true)
   			else
+  				puts "wtf mate"
   				result_arr.push(false)
   			end
   		end
@@ -113,8 +133,8 @@ class User < ApplicationRecord
   def day_score(date)
   	score_arr = []
   	puts "whatever this is..."
-  	p self.hourly_scores.where(created_at: date.midnight..date.end_of_day)
-  	scores = self.hourly_scores.where(created_at: date.midnight..date.end_of_day).to_a
+  	p self.hourly_scores.where(created_at: date.midnight.utc..date.end_of_day.utc).count
+  	scores = self.hourly_scores.where(created_at: date.midnight.utc..date.end_of_day.utc).to_a
   	score_hours = scores.map { |x| (x.created_at.utc.hour - 7) % 23 }
   	(0..23).each do |x|
   		if score_hours.include? x
